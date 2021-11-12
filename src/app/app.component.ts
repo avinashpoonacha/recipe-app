54d2e69287ff08d1c891ca2864df20be9eed7c9a
Developer <poonacha.avinash@gmail.com>
2021-11-12 13:33:23 -0500

    Components , databinding , view child , event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  menuItem:string='recipe';
+
+
+  onMenuItemClick(menuItemSelected:string){
+  this.menuItem = menuItemSelected;
+  }
 }
